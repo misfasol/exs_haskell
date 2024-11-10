dd6a07c1cca328778233c3c2d4f3c166c8baff7a
misfasol <gabriel.schlagenhaufer@gmail.com>
2024-11-09 21:44:53 -0300

    algoritmo shunting yard quase pronto falta verificar erro e testar

--- trab.hs
+++ trab.hs
@@ -42,14 +42,32 @@ prefixOf (x : xs) (y : ys) = x == y && prefixOf xs ys
 
 --          Tokens          Stack         Output        Resultado
 parser :: [TokenExpr] -> [TokenExpr] -> [TokenExpr] -> [TokenExpr]
-parser [] s o = o ++ reverse s
+parser [] s o = o ++ s
 parser [Var r] s o = parser [] s (o ++ [Var r])
-parser [x] a b = parser [] (x : a) b
+parser [AbParen] [] o = error "parenteses abrindo sem fechar"
+parser [FeParen] [] o = error "parenteses fechando sem um aberto"
+parser [x] [] o = parser [] [x] o
+parser [x] (s : ss) o
+  | precedencia x > precedencia s = parser [] (x : s : ss) o
+  | otherwise = parser [x] ss (o ++ [s])
 parser (Var r : xs) s o = parser xs s (o ++ [Var r])
-parser (x : xs) s o = parser xs (x : s) o
+parser (FeParen : xs) (s : ss) o
+  | precedencia FeParen == precedencia s = parser xs ss o
+  | otherwise = parser (FeParen : xs) ss (o ++ [s])
+parser (x : xs) [] o = parser xs [x] o
+parser (x : xs) (s : ss) o
+  | precedencia x > precedencia s = parser xs (x : s : ss) o
+  | otherwise = parser (x : xs) ss (o ++ [s])
+
+precedencia :: TokenExpr -> Int
+precedencia Nao = 4
+precedencia Conj = 3
+precedencia Disj = 2
+precedencia AbParen = 1
+precedencia FeParen = 1
 
 main = do
-  let str = "P ou Q ^ R"
+  let str = "(~P v Q) ^ R"
   print str
   let l = lexer str
   print l
