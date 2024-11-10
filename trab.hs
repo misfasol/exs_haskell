881355e317ac85a49f37567fed40bbb24115ba57
misfasol <gabriel.schlagenhaufer@gmail.com>
2024-11-10 16:41:43 -0300

    ta avaliando somente com V ou F

--- trab.hs
+++ trab.hs
@@ -1,21 +1,7 @@
--- data Expr
---   = Booleano Bool
---   | Var Char Bool
---   | Nao Expr
---   | Conj Expr Expr
---   | Disj Expr Expr
---   deriving (Show, Eq)
-
--- evalExpr :: Expr -> Bool
--- evalExpr (Booleano x) = x
--- evalExpr (Var x b) = b
--- evalExpr (Nao x) = not (evalExpr x)
--- evalExpr (Conj x y) = evalExpr x && evalExpr y
--- evalExpr (Disj x y) = evalExpr x || evalExpr y
-
 -- Tipos de token que a gente pode encontrar
 data TokenExpr
   = Var Char
+  | Booleano Bool
   | Nao
   | Conj
   | Disj
@@ -51,7 +37,7 @@ prefixOf (x : xs) (y : ys) = x == y && prefixOf xs ys
 
 --                Tokens          Stack         Output        Resultado
 shuntingYard :: [TokenExpr] -> [TokenExpr] -> [TokenExpr] -> [TokenExpr]
-shuntingYard [] [] o = o
+shuntingYard [] [] o = o -- caso base
 shuntingYard [] [s] o = shuntingYard [] [] (o ++ [s])
 shuntingYard [] (AbParen : ss) o = error "parenteses abriu sem fechar"
 shuntingYard [] (s : ss) o = shuntingYard [] ss (o ++ [s])
@@ -70,6 +56,7 @@ shuntingYard (x : xs) (s : ss) o
   | precedencia x > precedencia s = shuntingYard xs (x : s : ss) o
   | otherwise = shuntingYard (x : xs) ss (o ++ [s])
 
+-- verifica a precedencia do token
 precedencia :: TokenExpr -> Int
 precedencia Nao = 6
 precedencia Conj = 5
@@ -79,10 +66,63 @@ precedencia BiCond = 2
 precedencia AbParen = 1
 precedencia FeParen = 1
 
+avaliarCaso :: [TokenExpr] -> Caso
+avaliarCaso _ = Tautologia
+
+-- avalia uma lista de tokens que nao tem mais variaveis, somente V ou F e operadores
+avaliarExpr :: [TokenExpr] -> [TokenExpr] -> Bool
+avaliarExpr [] [] = error "input vazio"
+avaliarExpr [] [Booleano b] = b
+avaliarExpr x (Nao : Booleano b : ss) = avaliarExpr x (Booleano (not b) : ss)
+avaliarExpr x (Conj : Booleano a : Booleano b : ss) = avaliarExpr x (Booleano (a && b) : ss)
+avaliarExpr x (Disj : Booleano a : Booleano b : ss) = avaliarExpr x (Booleano (a || b) : ss)
+avaliarExpr x (Implica : Booleano a : Booleano b : ss) = avaliarExpr x (Booleano (not a || b) : ss)
+avaliarExpr x (BiCond : Booleano a : Booleano b : ss) = avaliarExpr x (Booleano (a == b) : ss)
+avaliarExpr (x : xs) s = avaliarExpr xs (x : s)
+
+-- troca todas as variaveis por V
+trocarPorV :: [TokenExpr] -> [TokenExpr]
+trocarPorV [] = []
+trocarPorV [Var r] = [Booleano True]
+trocarPorV [x] = [x]
+trocarPorV (Var r : xs) = Booleano True : trocarPorV xs
+trocarPorV (x : xs) = x : trocarPorV xs
+
+-- retorna lista somente com variaveis
+variaveisExpr :: [TokenExpr] -> [TokenExpr]
+variaveisExpr = filter eVariavel
+
+-- retorna se e variavel
+eVariavel :: TokenExpr -> Bool
+eVariavel (Var _) = True
+eVariavel _ = False
+
+-- qual caso a expressao e
+data Caso
+  = Tautologia
+  | Contradicao
+  | Contingente [TokenExpr] [TokenExpr]
+
+-- ainda nao sei oq fazer
+data ClausulaHorn = ClausulaHorn
+
+-- funcao principal
+funcaoPrincipal :: String -> (Caso, ClausulaHorn)
+funcaoPrincipal str = (caso, ClausulaHorn)
+  where
+    lexado = lexer str
+    dpsShunt = shuntingYard lexado [] []
+    caso = avaliarCaso dpsShunt
+
 main = do
-  let str = "P -> (Q v R)"
+  let str = "P v Q ^ R"
   print str
   let l = lexer str
   print l
   let p = shuntingYard l [] []
   print p
+  let a = [Var 'R', Var 'P', Conj]
+  print a
+  let trocado = trocarPorV a
+  print trocado
+  print $ avaliarExpr [Booleano False, Booleano False, Booleano True, Conj, Disj] []
