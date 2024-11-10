ea89b30145fa55b95ef8f92d143bb1da039eec2c
misfasol <gabriel.schlagenhaufer@gmail.com>
2024-11-09 22:24:57 -0300

    mudei o nome

--- trab.hs
+++ trab.hs
@@ -40,27 +40,25 @@ prefixOf [] _ = True
 prefixOf _ [] = False
 prefixOf (x : xs) (y : ys) = x == y && prefixOf xs ys
 
---          Tokens          Stack         Output        Resultado
-parser :: [TokenExpr] -> [TokenExpr] -> [TokenExpr] -> [TokenExpr]
-parser [] [] o = o
-parser [] [s] o = parser [] [] (o ++ [s])
-parser [] (AbParen : ss) o = error "parenteses abriu sem fechar"
-parser [] (s : ss) o = parser [] ss (o ++ [s])
-parser [Var r] s o = parser [] s (o ++ [Var r])
--- parser [AbParen] [] o = error "parenteses abrindo sem fechar"
--- parser [FeParen] [] o = error "parenteses fechando sem um aberto"
-parser [x] [] o = parser [] [x] o
-parser [x] (s : ss) o
-  | precedencia x > precedencia s = parser [] (x : s : ss) o
-  | otherwise = parser [x] ss (o ++ [s])
-parser (Var r : xs) s o = parser xs s (o ++ [Var r])
-parser (FeParen : xs) (s : ss) o
-  | precedencia FeParen == precedencia s = parser xs ss o
-  | otherwise = parser (FeParen : xs) ss (o ++ [s])
-parser (x : xs) [] o = parser xs [x] o
-parser (x : xs) (s : ss) o
-  | precedencia x > precedencia s = parser xs (x : s : ss) o
-  | otherwise = parser (x : xs) ss (o ++ [s])
+--                Tokens          Stack         Output        Resultado
+shuntingYard :: [TokenExpr] -> [TokenExpr] -> [TokenExpr] -> [TokenExpr]
+shuntingYard [] [] o = o
+shuntingYard [] [s] o = shuntingYard [] [] (o ++ [s])
+shuntingYard [] (AbParen : ss) o = error "parenteses abriu sem fechar"
+shuntingYard [] (s : ss) o = shuntingYard [] ss (o ++ [s])
+shuntingYard [Var r] s o = shuntingYard [] s (o ++ [Var r])
+shuntingYard [x] [] o = shuntingYard [] [x] o
+shuntingYard [x] (s : ss) o
+  | precedencia x > precedencia s = shuntingYard [] (x : s : ss) o
+  | otherwise = shuntingYard [x] ss (o ++ [s])
+shuntingYard (Var r : xs) s o = shuntingYard xs s (o ++ [Var r])
+shuntingYard (FeParen : xs) (s : ss) o
+  | precedencia FeParen == precedencia s = shuntingYard xs ss o
+  | otherwise = shuntingYard (FeParen : xs) ss (o ++ [s])
+shuntingYard (x : xs) [] o = shuntingYard xs [x] o
+shuntingYard (x : xs) (s : ss) o
+  | precedencia x > precedencia s = shuntingYard xs (x : s : ss) o
+  | otherwise = shuntingYard (x : xs) ss (o ++ [s])
 
 precedencia :: TokenExpr -> Int
 precedencia Nao = 4
@@ -74,5 +72,5 @@ main = do
   print str
   let l = lexer str
   print l
-  let p = parser l [] []
+  let p = shuntingYard l [] []
   print p
